723c2053753c9aa62595b0295bc8ce603b154e49
M Codes <106161316+Mfonido-abasi@users.noreply.github.com>
2025-01-30 12:04:59 +0000

    final1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+Mastercard Validator
 
+Description
+
+This is a simple credit card validation system that specifically checks if a given card number belongs to a Mastercard. If the card is a Mastercard, it returns the number, otherwise, it returns null. However, it still extracts and displays other card details.
+
+Mastercard numbers typically start with 51 to 55 or 2221 to 2720.
+
+The function checks the card number against these prefixes.
+
+If the number matches, it returns the full card details; otherwise, it returns null.
--- Regex-Task6.js
+++ Regex-Task6.js
@@ -1,8 +1,8 @@
 /* System to Validate the details of a Master card. 
 This system is designed to validate only Mastercard. Any other card type would return null.*/
 
-const cardNum = "5234 5678 9087 5432";
-const masterCardNum = /^(?:2|5)\d{3} ?\d{4} ?\d{4} ?\d{4}$/.exec(cardNum);
+const cardNum = "2700 5678 9087 5432";
+const masterCardNum = /^(?:5[1-5]\d{2}|222[1-9]|22[3-9]\d|2[3-6]\d{2}|27[1]\d|27[0]\d|2720) ?\d{4} ?\d{4} ?\d{4}$/.exec(cardNum);
 const cardName = /^\S+ ?\S+$/.exec ("Mfonido Codes");
 const cardExpDate = /^(?:0[1-9]|1[0-2])[\/ ]\d{2}$/.exec ("12/25");
 const cardSecurityNum = /^\d{3}$/.exec(123);
@@ -14,6 +14,7 @@ if(masterCardNum){
 else{
     console.log("Not Mastercard");
 }
+
 console.log("\nMaster Card Number:");
 console.log(masterCardNum);
 console.log("\nName on Card:");
